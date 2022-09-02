d3470f0671d27e6eefdc9cfe7aebe0eaa7193cbe
satishrapolu84 <67561238+satishrapolu84@users.noreply.github.com>
2022-09-02 06:25:01 -0400

    amplify config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "perdevuiamplifysr9ac4200c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
